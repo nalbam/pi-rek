4f33e941829595b565a772a806f26d710eb3a890
nalbam <me@nalbam.com>
2018-11-09 18:46:07 +0900

    substring

--- scan.sh
+++ scan.sh
@@ -8,4 +8,5 @@ IMAGE=${SRC_DIR}/captures/images/image.jpg
 raspistill -w 960 -h 720 -t 900 -th none -x none -o ${IMAGE}
 
 # zbar img
-zbarimg ${IMAGE} 2>&1 | grep 'QR-Code' > ${SRC_DIR}/static/qr.json
+QR=$(zbarimg ${IMAGE} 2>&1 | grep 'QR-Code')
+echo ${QR:8} > ${SRC_DIR}/static/qr.json

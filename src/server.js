1f6ad26045f7423bfc9439327c01765ddce3217b
nalbam <me@nalbam.com>
2018-11-09 17:26:09 +0900

    add server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
+const os = require('os'),
+    ip = require('ip'),
+    moment = require('moment-timezone'),
+    express = require('express'),
+    request = require('request');
+
 const path = require('path');
 const MotionDetectionModule = require('pi-motion-detection');
 
 const motionDetector = new MotionDetectionModule({
     captureDirectory: path.resolve(__dirname, 'captures'),
+});
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/favicon.ico', express.static('views/favicon.ico'));
+
+app.get('/', function (req, res) {
+    let host = os.hostname();
+    let date = moment().tz('Asia/Seoul').format();
+    res.render('index.ejs', {host: host, date: date, server: ip.address(), client: req.ip.split(':').pop()});
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!');
 });
 
 motionDetector.on('motion', () => {

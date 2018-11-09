273b14404d54b125dcd02b4b81419e7e5ad40941
nalbam <me@nalbam.com>
2018-11-09 20:17:32 +0900

    800 600

--- scan.sh
+++ scan.sh
@@ -5,7 +5,7 @@ SRC_DIR=~/rpi-rek/src
 IMAGE=${SRC_DIR}/captures/images/image.jpg
 
 # raspi still
-raspistill -w 960 -h 720 -t 900 -th none -x none -o ${IMAGE}
+raspistill -w 800 -h 600 -t 900 -th none -x none -o ${IMAGE}
 
 # qr
 # zbarimg ${IMAGE} 2>&1 | grep 'QR-Code' > ${SRC_DIR}/static/qr.json

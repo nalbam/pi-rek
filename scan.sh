900245bd1e684e0838336b86d0ae7e14c3af5923
nalbam <me@nalbam.com>
2018-11-09 20:16:18 +0900

    qr

--- scan.sh
+++ scan.sh
@@ -7,8 +7,12 @@ IMAGE=${SRC_DIR}/captures/images/image.jpg
 # raspi still
 raspistill -w 960 -h 720 -t 900 -th none -x none -o ${IMAGE}
 
-# zbar img
-zbarimg ${IMAGE} 2>&1 | grep 'QR-Code' > ${SRC_DIR}/static/qr.json
+# qr
+# zbarimg ${IMAGE} 2>&1 | grep 'QR-Code' > ${SRC_DIR}/static/qr.json
 
-# QR=$(zbarimg ${IMAGE} 2>&1 | grep 'QR-Code')
-# echo ${QR:8} > ${SRC_DIR}/static/qr.json
+QR=$(zbarimg ${IMAGE} 2>&1 | grep 'QR-Code')
+if [ ! -z ${QR} ]; then
+    echo "${QR:8}" > ${SRC_DIR}/static/qr.json
+else
+    echo "" > ${SRC_DIR}/static/qr.json
+fi

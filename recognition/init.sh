075474391c3c3be2356c6d6f6c367ac18515ba0c
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2020-02-21 02:05:35 +0900

    add

---
+++ recognition/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo pip3 install cmake
+sudo pip3 install face_recognition
+sudo pip3 install imutils
+sudo pip3 install opencv-python
+sudo pip3 install opencv-python-headless

91ea9090410344c8888f4667845cfe7c9415bb7d
Jungyoul Yu <me@nalbam.com>
2020-02-21 03:00:08 +0900

    Update init.sh

--- recognition/init.sh
+++ recognition/init.sh
@@ -8,3 +8,8 @@ sudo pip3 install face_recognition
 sudo pip3 install imutils
 sudo pip3 install opencv-python
 sudo pip3 install opencv-python-headless
+
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install python3-pyqt5
